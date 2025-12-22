3cf3ef8824f5547f3946dfa6cc2650fe3480be36
mehdi <mehdi@gmail.com>
2025-12-22 10:11:10 +0330

    create school app models

--- school/models.py
+++ school/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class AcademicYear(models.Model):
+    title = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_current = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+class Grade(models.Model):
+    name = models.CharField(max_length=255)
+    level = models.IntegerField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class SchoolClass(models.Model):
+    year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    Subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    section = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    
+
