f6ed8fef61e307b818b680a85c5fe80dd536274c
mehdi <mehdi@gmail.com>
2026-05-26 09:28:19 +0330

    edit

--- accounts/views.py
+++ accounts/views.py
@@ -7,25 +7,28 @@ from django.contrib.auth import logout, login, authenticate
 # Create your views here.
 
 def login_form(request):
-    if request.method == 'POST':
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                         username=cd['username'],
-                         password=cd['password'])
-            if user is not None:
-                login(request, user)
-                messages.success(request, 'با موفقیت وارد شدید')
-                return redirect('core:dashboard')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = LoginForm(request, data=request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                user = authenticate(request,
+                            username=cd['username'],
+                            password=cd['password'])
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'با موفقیت وارد شدید')
+                    return redirect('core:dashboard')
+                else:
+                    messages.error(request, 'نام کاربری یا رمز عبور اشتباه است')
             else:
-                messages.error(request, 'نام کاربری یا رمز عبور اشتباه است')
-        else:
-            messages.error(request, 'خطای فرم را بررسی کنید')
+                messages.error(request, 'خطای فرم را بررسی کنید')
 
+        else:
+            form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
     else:
-        form = LoginForm()
-    return render(request, 'accounts/login.html', {'form': form})
+        return redirect('core:dashboard')
 
 def auth_logout(request):
     logout(request)
